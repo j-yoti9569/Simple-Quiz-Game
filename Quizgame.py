1929fd5a1f5b453c9d74c2e60efafaeee7e50752
j-yoti9569 <23jyoti2005@gmail.com>
2025-03-30 19:09:19 +0530

    Add files via upload

---
+++ Quizgame.py
@@ -0,0 +1,43 @@
+score=0
+ans=input("who develop python?")
+if ans.lower()=="guido van rossum":
+    print("correct")
+    score=score+1
+else:
+    print("incorrect")    
+    
+ans=input("who is father of computer?")
+if ans.lower()=="charles babbage":
+    print("correct")
+    score=score+1
+else:
+    print("incorrect")  
+    
+ans=input("who does 'IDE'stand for")
+if ans.lower()=="integrated development environment":
+    print("correct")
+    score=score+1
+else:
+    print("incorrect")    
+    
+  
+ans=input("what does 'ALU' stand for?")
+if ans.lower()=="airthmetic logic unit":
+    print("correct")
+    score=score+1
+else:
+    print("incorrect")    
+    
+    
+ans=input("who does 'CPU' stand for?")
+if ans.lower()=="central processing unit":
+    print("correct")
+    score=score+1
+else:
+    print("incorrect")   
+    
+    
+print("your score is",score)
+              
+    
+        
